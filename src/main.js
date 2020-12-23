4d3ab466679859dc53fc9ca1f09d3c07c3175c52
OUMAREGA <oumarmarega18@gmail.com>
2020-12-23 22:19:18 +0100

    update ssr - downgrade puppeter

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import axios from 'axios'
 Vue.prototype.$axios = axios;
 Vue.prototype.$url_devise_key= '1bc7a3306ba06819c6fc';
 Vue.prototype.$url_news_key= 'b91aaef1b9ab4235b6fe1766bff8d3e2';
-Vue.prototype.$url_news = 'http://newsapi.org/v2/top-headlines?country=fr&category=technology&apiKey=b91aaef1b9ab4235b6fe1766bff8d3e2';
+Vue.prototype.$url_news = 'https://newsapi.org/v2/top-headlines?country=fr&category=technology&apiKey=b91aaef1b9ab4235b6fe1766bff8d3e2';
 Vue.prototype.$url_devise = 'https://free.currconv.com/api/v7/convert';
 Vue.prototype.$own_api_url = 'https://boiling-escarpment-06159.herokuapp.com'
 
